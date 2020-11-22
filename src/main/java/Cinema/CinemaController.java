a97c4583071aec7600d296983e0618f5c609687b
david02653 <david02653@gmail.com>
2020-11-23 01:11:33 +0800

    commit test

--- src/main/java/Cinema/CinemaController.java
+++ src/main/java/Cinema/CinemaController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class CinemaController {
-	// for commit test
 	
 	@CrossOrigin(origins = "*")
 	@RequestMapping("/")
